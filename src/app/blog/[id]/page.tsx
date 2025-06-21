39ec047363f2b81630db34979a94d4f9baa35bf5
Mauricio Fernandez <mauricio@broobe.com>
2025-06-20 22:02:59 -0300

    arrow in blogs to handle navigation

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -7,18 +7,37 @@ import {
   LenguajesDelAmor,
   EnBuscaDelEquilibro,
 } from "@/sections/blog/list";
+import { useRouter } from "next/navigation";
+import blogList from "../../../sections/blog/blog.json";
+import { useEffect } from "react";
 
 export default function BlogDetail() {
   const params = useParams();
   const id = params?.id;
+  const router = useRouter();
+  const handleNextBlog = () => {
+    const nextBlog = blogList.find((blog) => blog.id === Number(id) + 1);
+    if(!nextBlog) return
+    router.replace(`/blog/${Number(id) + 1}`)
+  }
+  const handlePreviousBlog = () => {
+    const previousBlog = blogList.find((blog) => blog.id === Number(id) - 1);
+    if(!previousBlog) return
+    router.replace(`/blog/${Number(id) - 1}`)
+  }  
+
   return (
-    <main className="">
+    <main>
       <div className="max-w-[1024px] m-auto pt-10 pb-20">
       {id === "1" ? <ComoMantenerConversacionesDificiles /> : null}
       {id === "2" ? <ComoEmpezarTerapia /> : null}
       {id === "3" ? <LaDesmotivacionLaboral /> : null}
       {id === "4" ? <LenguajesDelAmor /> : null}
       {id === "5" ? <EnBuscaDelEquilibro /> : null}
+      </div>
+      <div className="flex justify-center gap-4 mb-20 font-baby-doll text-[20px]"> 
+        <button onClick={handlePreviousBlog}>{'< Anterior'}</button>
+        <button onClick={handleNextBlog}>{'Siguiente >'}</button>
       </div>
     </main>
   );
